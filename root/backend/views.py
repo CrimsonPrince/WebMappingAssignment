9d82ebd8b48bb4da153ce0502b81ca195c7f4241
CrimsonPrince <arthur1coll@gmail.com>
2019-10-27 00:41:30 +0100

    Testing new Github Actions CI

--- root/backend/views.py
+++ root/backend/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello is it me you are looking for?")
+    return HttpResponse("CI TEST")
 
 def detail(request):
     return HttpResponse("Hiya")

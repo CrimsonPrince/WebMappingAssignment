7217d164375485095b7efe4d325a398fb7185073
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-10-30 20:10:34 +0000

    Added GEO Settings

--- root/backend/views.py
+++ root/backend/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('CI Test')
+    return render(request, 'index.html')
 
 def detail(request):
     return HttpResponse("Hiya")

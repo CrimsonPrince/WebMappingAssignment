4d0c5b42cb007b730d54b1015e1b1d5a96172a29
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-11-03 23:48:30 +0000

    Added GeoJSon Serialization

--- root/backend/urls.py
+++ root/backend/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     path('details', views.detail, name="detail"),
+    path('planning', views.GeojsonLocationList.as_view()),
 ]
--- root/backend/views.py
+++ root/backend/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
+from . import serializers
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework_gis.pagination import GeoJsonPagination
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Coolio")
+class GeojsonLocationList(ListCreateAPIView):
+    queryset = models.Planning.objects.all()
+    serializer_class = serializers.PlanningSerializer
+    pagination_class = GeoJsonPagination
 
 def detail(request):
+    return HttpResponse("Hiya")
+
+def index(request):
     return HttpResponse("Hiya")

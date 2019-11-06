e4fa2e7b5ab6cfe589334628b653900a7ec8df77
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-11-06 15:25:17 +0000

    Reverting to Hopefully fix PWA

--- frontend/src/app/list/list.page.ts
+++ frontend/src/app/list/list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
@@ -8,26 +11,15 @@ import { Component, OnInit } from '@angular/core';
 export class ListPage implements OnInit {
   private selectedItem: any;
   private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
     'build'
   ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  public items: Array<{ }> = [];
+
+  constructor(public http: HttpClient,
+    public plt: Platform,
+    public router: Router) {
+        this.http.get('https://web.r4.ie/planning').subscribe(planning => this.items.push(planning));
+        console.log(this.items);
   }
 
   ngOnInit() {

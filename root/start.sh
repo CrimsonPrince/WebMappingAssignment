a89689a525e9a3db66492b3a6887a74f76583b35
CrimsonPrince <arthur1coll@gmail.com>
2019-12-13 13:42:00 +0000

    Added --noinput flag

--- root/start.sh
+++ root/start.sh
@@ -4,7 +4,7 @@ set -v
 echo "Startup Script"
 
 echo "Creating Migrations"
-python manage.py makemigrations
+python manage.py makemigrations --noinput
 echo "Applying Migrations"
 python manage.py migrate
 # Start Supervisor, with Nginx and uWSGI

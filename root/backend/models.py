69544eaedde4790f800da03a3c5c6bf92f626225
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-12-13 12:59:48 +0000

    Added States Data

--- root/backend/models.py
+++ root/backend/models.py
@@ -2,19 +2,9 @@ from django.contrib.gis.db import models
 from django.contrib.gis.db import models as geomodels
 
 class ElectoralDivision(models.Model):
-    OBJECTID = models.CharField(max_length=100, blank=False)
-    LE_ID = models.CharField(max_length=100)
-    LE_ENGLISH = models.CharField(max_length=10)
-    LE_GAEILGE = models.CharField(max_length=100)
-    COUNTY = models.CharField(max_length=10, blank=True, null=True)
-    CONTAE = models.CharField(max_length=10)
-    PROVINCE = models.CharField(max_length=10)
-    CENTROID_X = models.CharField(max_length=2)
-    CENTROID_Y = models.CharField(max_length=10)
-    GUID = models.CharField(max_length=1000)
-    ObjectID_1 = models.CharField(max_length=1000)
-    Shape_Area = models.CharField(max_length=1000, blank=True, null=True)
-    Shape_Length = models.CharField(max_length=1000, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=False)
+    density = models.CharField(max_length=100)
+    id = models.CharField(max_length=10)
     geometry = geomodels.MultiPolygonField()
 
     class Meta:
